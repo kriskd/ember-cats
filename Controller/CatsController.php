db6b7b139e382553bd1767264a3368d7396c77d9
Kris Dockter <kriskd@gmail.com>
2015-01-19 19:19:09 -0600

    Format json correctly

--- Controller/CatsController.php
+++ Controller/CatsController.php
@@ -4,7 +4,31 @@ App::uses('AppController', 'Controller');
 class CatsController extends AppController {
 
     public function index() {
-        $cats = array('Casper', 'Tux', 'Domino', 'Neo', 'Wynn', 'Sadie');
+        $this->response->header(array(
+            'Access-Control-Allow-Origin: *',
+            ));
+        $cats = ['cats' => [
+                [
+                    'id' => 1,
+                    'name' => 'Casper', 
+                ],[
+                    'id' => 2,
+                    'name' => 'Tux', 
+                ],[
+                    'id' => 3,
+                    'name' => 'Domino', 
+                ],[
+                    'id' => 4,
+                    'name' => 'Neo', 
+                ],[
+                    'id' => 5,
+                    'name' => 'Wynn', 
+                ],[
+                    'id' => 6,
+                    'name' => 'Sadie',
+                ],
+            ]
+        ];
         $this->set('cats', json_encode($cats));
         $this->layout = 'ajax';
     }
