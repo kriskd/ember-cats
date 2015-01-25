2b2c92e714fce1c16df18dcdd15f651437bb6a67
Kris Dockter <kriskd@gmail.com>
2015-01-25 10:16:22 -0600

    No need to define path to json

--- ember-cats/app/models/cats.js
+++ ember-cats/app/models/cats.js
@@ -1,10 +1,6 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string')
 });
 
-Ember.Store = DS.Store.extend({
-  url: 'http://ember-cats.dev/cats.json'
-});
