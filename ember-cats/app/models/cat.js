f2290f99bc8f751c8369efa94b4c82c017df753f
Kris Dockter <kriskd@gmail.com>
2015-01-25 10:35:49 -0600

    Clean up unneeded code

--- ember-cats/app/models/cat.js
+++ ember-cats/app/models/cat.js
@@ -1,10 +1,4 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
-});
-
-Ember.Store = DS.Store.extend({
-  url: 'http://ember-cats.dev/cats.json'
 });
